b50facb63962e17c14dfb4eef1770fb97a53f884
SCrabtree <sean.crabtree@gmail.com>
2015-03-09 17:03:41 -0700

    Fleeting 1.0+

--- Fleeting/Today.swift
+++ Fleeting/Today.swift
@@ -9,13 +9,105 @@ import UIKit
 
 struct Current {
     
+    var currentTime: String?
     var temperature: Int
+    var humidity: Double
+    var precipProbability: Double
     var summary: String
     var icon: UIImage?
+    // var moonPhase: UIImage?
     
     init(weatherDictionary: NSDictionary) {
         let currentWeather = weatherDictionary["currently"] as NSDictionary
+        
         temperature = currentWeather["temperature"] as Int
+        humidity = currentWeather["humidity"] as Double
+        precipProbability = currentWeather["precipProbability"] as Double
         summary = currentWeather["summary"] as String
+        
+        let currentTimeIntValue = currentWeather["time"] as Int
+        currentTime = dateStringFromUnixTime(currentTimeIntValue)
+        
+//        let iconString = currentWeather["icon"] as String
+//        icon = weatherIconFromString(iconString)
+    }
+    
+    func dateStringFromUnixTime(unixTime: Int) -> String {
+        
+        let timeInSeconds = NSTimeInterval(unixTime)
+        let weatherDate = NSDate(timeIntervalSince1970: timeInSeconds)
+        
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.timeStyle = .ShortStyle
+        
+        return dateFormatter.stringFromDate(weatherDate)
     }
 }
+/*
+struct ArtWork {
+    
+    let artworkArray = [
+        
+        
+        
+        
+        
+    ]
+        
+    func randomArt() -> Image {
+        // var randomNumber = Int(arc4random_uniform(UInt32(factsArray.count)))
+            
+        var unsignedArrayCount = UInt32(artworkArray.count)
+        var unsignedRandomNumber = arc4random_uniform(unsignedArrayCount)
+        var randomNumber = Int(unsignedRandomNumber)
+        
+        return artworkArray[randomNumber]
+    }
+}
+*/
+
+
+
+
+
+
+
+
+/* IMAGE PER WEATHER TYPE
+    func weatherIconFromString(stringIcon: String) -> UIImage {
+        var imageName: String
+        
+        switch stringIcon {
+        case "clear":
+            imageName = "clear-day"
+        case "clear-night":
+            imageName = "clear-night"
+        case "rain":
+            imageName = "rain-snow"
+        case "snow":
+            imageName = "snow"
+        case "sleet":
+            imageName = "cloudy-sleet"
+        case "wind":
+            imageName = "wind-fog"
+        case "fog":
+            imageName = "wind-fog"
+        case "cloudy":
+            imageName = "cloudy-sleet"
+        case "partly-cloudy-day":
+            imageName = "partly-cloudy"
+        case "partly-cloudy-night":
+            imageName = "cloudy-night"
+        case "hail":
+            imageName = "hail-thunderstorm-tornado"
+        case "thunderstorm":
+            imageName = "hail-thunderstorm-tornado"
+        case "tornado":
+            imageName = "hail-thunderstorm-tornado"
+        default:
+            imageName = "default"
+        }
+    
+        var iconImage = UIImage(named: imageName)
+        return iconImage!
+    }   */
--- Fleeting/ViewController.swift
+++ Fleeting/ViewController.swift
@@ -17,13 +17,21 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
     var userLatitude : Double!
     var userLongitude : Double!
     
+//    @IBOutlet weak var iconView: UIImageView!
+//    @IBOutlet weak var precipitationLabel: UILabel!
+//    @IBOutlet weak var currentTimeLabel: UILabel!
     @IBOutlet weak var refreshButton: UIButton!
     @IBOutlet weak var refreshActivityIndicator: UIActivityIndicatorView!
     
     
+    @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var summaryLabel: UILabel!
     @IBOutlet weak var mainArt: UIImageView!
+    @IBOutlet weak var sunriseTime: UILabel!
+    @IBOutlet weak var sunsetTime: UILabel!
+    @IBOutlet weak var moonPhase: UILabel!
+
     
     let apiKey = "d7539a30efd5669aa702bdce4a2e1436"
     
@@ -105,6 +113,7 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
         userLocation = "\(userLatitude),\(userLongitude)"
         
         let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
+        //let forecastURL = NSURL(string: "34.017547,-118.493111", relativeToURL: baseURL)
         let forecastURL = NSURL(string: "\(userLocation)", relativeToURL: baseURL)
 println("### userLocation: \(forecastURL)")
         
@@ -119,6 +128,7 @@ println("### userLocation: \(forecastURL)")
                 let currentWeather = Current(weatherDictionary: weatherDictionary)
                 dispatch_async(dispatch_get_main_queue(), { () -> Void in
                     self.temperatureLabel.text = "\(currentWeather.temperature)"
+                    //self.precipitationLabel.text = "\(currentWeather.precipProbability)"
                     self.summaryLabel.text = "\(currentWeather.summary)"
                 })
             } else{
@@ -147,10 +157,25 @@ println("### userLocation: \(forecastURL)")
     
     func initArtwork() {
         let r = Int(arc4random())
-        let artId = r % 12 + 1;
+        let artId = r % 13 + 1;
         let artIdString = "\(artId)"
         self.mainArt.image = UIImage(named: artIdString)
     }
+/*
+    @IBAction func refresh() {
+        
+        refreshButton.hidden = true
+        refreshActivityIndicator.hidden = false
+        refreshActivityIndicator.startAnimating()
+        
+        getCurrentWeatherData()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+*/
     
 }
 
